d54a5c9740570ea70c643328eb3730e8b2eb31e2
yumicha1 <36577595+yumicha1@users.noreply.github.com>
2021-07-03 00:35:14 -0400

    how to use variable

---
+++ src/main.go
@@ -0,0 +1,22 @@
+package learnNewLanguage
+
+import "fmt"
+
+func main()  {
+	fmt.Println("Hello World!") 
+
+	var i,j = 8, 42
+	fmt.Println("i = ")
+    fmt.Println(i)
+    fmt.Println("j = ")
+    fmt.Println(j)
+    fmt.Println("")
+    
+    k,l:=10,5
+    fmt.Println("k = ")
+    fmt.Println(k)
+    fmt.Println("l = ")
+    fmt.Println(l)
+
+
+}
